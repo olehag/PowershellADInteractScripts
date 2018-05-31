fcc7b426cfe3c5a76473844fd8b6c5f31e11e4e6
LordHagen <olehag04@nfk.no>
2018-05-31 12:08:32 +0200

    edits

--- PasswordScripts/ChangePassword ADuser.ps1
+++ PasswordScripts/ChangePassword ADuser.ps1
@@ -1,15 +1,12 @@
 $ErrorActionPreference = 'stop'
-
+#Authenticate with AD Admincredentials.
 $creds = Get-Credential $env:USERDOMAIN\$env:USERNAME-admin
-
+#Info about the user.
 $username = Read-Host -Prompt 'User to change!'
-
 $oldpassword = Read-Host -AsSecureString -Prompt 'Enter your old password!'
-
 $newpassword = Read-Host -AsSecureString -Prompt 'Enter your new password!'
-
+#Password change execution.
 Set-ADAccountPassword  -Identity $username -Credential $creds -OldPassword $oldpassword -NewPassword $newpassword
-
 #Exit
 Write-Host "`tPress any key to exit..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
